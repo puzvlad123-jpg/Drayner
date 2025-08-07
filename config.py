18c2717839094dbec331b999b529736babaa4e1f
puzvlad123-jpg <puzvlad123@gmail.com>
2025-08-08 00:25:52 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = "8341745055:AAETEOVSgGrzouOdMsebCCQdp26_eORDCmQ"
-ADMIN_ID = [905228760, 7496041986]
+ADMIN_ID = 905228760
 SENDER_ID = 7183020291
 GPT_PROMPT = "Ты бизнесбот в телеграмм. Ты должке отвечать когда пользователь не в сети. Отвечай не длинными ответами и не форматируй текст.\nЗапрос: "
 BOT_USERNAME = "SendChecsBot"
